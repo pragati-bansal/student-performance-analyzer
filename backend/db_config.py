7ffab2914d96cb8caacc4327ccd46098a4e52a9c
Pragati Bansal <pragatibansal20@gmail.com>
2026-02-05 19:18:54 +0530

    Add MySQL connection configuration

--- backend/db_config.py
+++ backend/db_config.py
@@ -1,1 +1,10 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="StrongPassword123",
+        database="student_analyzer"
+    )
 

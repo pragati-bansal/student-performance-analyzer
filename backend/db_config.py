8732d7b4f784822a3f10dc9188489a964ac20fa1
Pragati Bansal <pragatibansal20@gmail.com>
2026-02-06 14:33:28 +0530

    Update db_config.py

--- backend/db_config.py
+++ backend/db_config.py
@@ -1,10 +1,14 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="StrongPassword123",
-        database="student_analyzer"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
     )
-
